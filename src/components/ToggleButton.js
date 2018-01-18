d3995efe7a0a373a5b0aa398f766eac681845873
Nicholas D'Amico <nickalan82@icloud.com>
2018-01-18 14:56:17 -0500

    add constructor method, toggleButton class method

--- src/components/ToggleButton.js
+++ src/components/ToggleButton.js
@@ -1,1 +1,25 @@
 // Component Code Goes Here
+import React from 'react';
+
+export class ToggleButton extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isEnabled: false
+    }
+    this.toggleButton = this.toggleButton.bind(this);
+  }
+
+  toggleButton() {
+    const newState = this.state.isEnabled ? false : true;
+    this.setState({ isEnabled: newState })
+  }
+
+  render() {
+    return(
+      <button className="toggle-button" onClick={this.toggleButton}>
+        I am Toggled {this.state.isEnabled ? 'on' : 'off'}
+      </button>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ToggleButton } from './components/ToggleButton';
+import { Address } from './components/Address';
 
 ReactDOM.render(
-  <div>Components Need To Be Mounted</div>,
+  <div>
+  <Address street="321 Autumn Lane" city="Gatlinuburg" />
+  <ToggleButton />
+  </div>,
   document.getElementById('root')
 )
